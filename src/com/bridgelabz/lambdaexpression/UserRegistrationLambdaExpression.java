76c1e59c802c4cc473f2f43b08a4aa2e74035355
ktritsh7 <ktriesh7@gmail.com>
2021-07-05 15:28:30 +0530

    Welcome to User Registration problem using lambda expression

---
+++ src/com/bridgelabz/lambdaexpression/UserRegistrationLambdaExpression.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.lambdaexpression;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistrationLambdaExpression {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Using Lambda Expression");
+    }
+}
